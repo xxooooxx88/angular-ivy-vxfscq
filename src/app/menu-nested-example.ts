b5583e27753d25ebc5af540f3c70f89089b461b7
xxooooxx88 <xxooooxx@88.com>
2022-07-18 20:26:52 +0800

    Initial commit

---
+++ src/app/menu-nested-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Nested menu
+ */
+@Component({
+  selector: 'menu-nested-example',
+  templateUrl: 'menu-nested-example.html',
+})
+export class MenuNestedExample {}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
